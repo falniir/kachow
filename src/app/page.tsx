114a87ec6ca240e16639bf589afb295959a5d660
Eduard Andrei Cristea <falniir@gmail.com>
2024-02-01 20:08:02 +0100

    added a dialog/modal thingy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import Particles from "@/components/particles";
 import Image from "next/image";
 import landingPagePic from "@/lib/images/gun.gif";
+import { Dialog } from "@/components/ui/dialog";
+import { DialogClose } from "@radix-ui/react-dialog";
+import DialogPage from "@/components/dialog";
 const navigation = [
   { name: "Projects", href: "/projects" },
   { name: "Contact", href: "/contact" },
@@ -13,14 +18,14 @@ export default function Home() {
 
 
   return (
-      <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-pink-950 via-pink-950/30 to-white">
-        <nav className="my-16 animate-fade animate-once animate-duration-[3000ms] ">
+      <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-pink-900 via-pink-950/40 to-pink-900">
+        <nav className="my-16 animate-fade-right animate-once animate-duration-[3000ms] ">
           <ul className="flex items-center justify-center gap-4">
             {navigation.map((item) => (
               <Link
                 key={item.href}
                 href={item.href}
-                className="text-sm animate-fade animate-once animate-duration-[3000ms] text-slate-500 hover:text-slate-300"
+                className="text-sm animate-fade-right animate-once animate-duration-[3000ms] text-black hover:text-slate-300"
               >
                 {item.name}
               </Link>
@@ -28,23 +33,27 @@ export default function Home() {
 
           </ul>
         </nav>
-        <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-slate-300/0 via-slate-300/50 to-slate-300/0" />
+        <div className="hidden w-screen h-px animate-ease-in-out md:block animate-fade-left animate-duration-[3000ms] bg-gradient-to-r from-slate-300/0 via-slate-300/50 to-slate-300/0" />
         <Particles
-          className="absolute inset-0 -z-10 animate-fade"
+          className="absolute inset-0 -z-10 animate-fade-right"
           quantity={500}
         />
-        <h1 className="z-10 text-4xl text-transparent animate-fade bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+        <h1 className="z-10 text-4xl text-transparent animate-fade-right animate-once animate-duration-[3000ms] animate-normal bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
           Become my valentine?
 
         </h1>
 
-        <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-        <div className="my-16 text-center animate-fade-in">
-          <h2 className="text-2xl text-zinc-500">
+        <div className="hidden w-screen h-px md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+        <div className="my-16 text-center">
+          <h2 className="text-2xl text-white">
             Or else...
-            <Image
+          <Image
+            className="animate-fade animate-once animate-duration-[10000ms] animate-normal"
               src={landingPagePic}
-              alt="Picture of the author"
+            alt="Picture of the author"
+            width={500}
+            height={500}
+              
             />
           
           </h2>
@@ -52,6 +61,7 @@ export default function Home() {
            <button className="px-4 py-2 mt-4 text-sm font-bold text-white transition duration-500 bg-slate-500 rounded-full hover:bg-slate-400 hover:text-slate-300">NO :/</button>
           <button className="ml-14 px-4 py-2 mt-4 text-sm font-bold text-white transition duration-500 bg-zinc-500 rounded-full hover:bg-zinc-400 hover:text-zinc-300 space-x-50">YES!</button>
 
+          <DialogPage/>
           </div>
 
         </div>
