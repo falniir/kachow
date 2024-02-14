843d270349cf6ea8fff6da2518f5e16c66a34a3b
Eduard Andrei Cristea <falniir@gmail.com>
2024-02-14 03:31:40 +0100

    added metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
           <div className="flex flex-row items-center justify-center ">
            <button onClick={handleNoButtonClick} className="px-4 py-2 mt-4 text-sm font-bold text-white transition duration-500 bg-slate-500 rounded-full hover:bg-slate-400 hover:text-slate-300">NO :/</button>
           <DialogPage scale={dialogScale}
-            dialogText="YES!" dialogTitle="YAY!" dialogDescription="I love you too! <3"
+            dialogText="YES!" dialogTitle="YAY!" dialogDescription="I love you too! 💝"
             
           />
           </div>
